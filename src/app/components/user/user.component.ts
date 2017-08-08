308326a7aa04a168a19ebde8b5a6c55dd4ee0991
Jess <mcanally.jess@gmail.com>
2017-08-07 23:42:47 -0400

    added examples of Two-way binding, service, routing, and some directives

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-user',
@@ -12,24 +13,60 @@ export class UserComponent implements OnInit {
   address:Address;
   hobbies:string[];
   hello:any;
+  posts:Post[];
+  isEdit:boolean = false;
 
-  constructor() {
+  constructor(private dataService:DataService) {
     console.log('constructor ran..');
-   }
+
+  }
 
   ngOnInit() {
-    console.log('ngOnInit ran..');
+    console.log('ngOnInit ran...');
 
     this.name = 'Julio Jones';
+    this.email = 'bestWRAllTime@HOF.com';
     this.age = 30;
-    this.email = 'greatestWRofAllTime@gmail.com';
     this.address = {
-      street:'50 Main st',
-      city:'Atlanta',
+      street:'1234 Falcons Dr',
+      city: 'Atlanta',
       state:'GA'
-    };
-    this.hobbies = ['Write code','Watch movies','Listen to music'];
-    this.hello = 'hello';
+    }
+    this.hobbies = ['Write code', 'Watch movies', 'Listen to music'];
+    this.hello ='Hai there';
+
+    this.dataService.getPosts().subscribe((posts) => {
+      //console.log(posts);
+      this.posts = posts;
+    });
+  }
+
+  onClick(){
+    this.name='Matt Bryant';
+    this.hobbies.push('New Hobby');
+  }
+
+  addHobby(hobby){
+    console.log(hobby);
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+
+  deleteHobby(i){
+    this.hobbies.splice(i, 1);
+  }
+
+  // deleteHobby(hobby){
+  //   console.log(hobby);
+  //   for(let i=0;i<this.hobbies.length;i++){
+  //     if(this.hobbies[i] == hobby){
+  //       this.hobbies.splice(i,1);
+  //     }
+  //   }
+  // }
+
+  toggleEdit(){
+    this.isEdit = !this.isEdit;
   }
 
 }
@@ -40,3 +77,12 @@ interface Address{
   city:string,
   state:string
 }
+
+interface Post{
+  id: number,
+  title:string,
+  body:string,
+  userId:number
+}
+
+
